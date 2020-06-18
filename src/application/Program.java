967785867a53682d5e086570cb02bff776015b98
CAIO DEL BONNE <caiocesar_01@hotmail.com>
2020-06-18 17:44:23 -0300

    FindBy id

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(20, "Bob", "bob@bob", new Date(), 3000.0, obj);
-		
+//		Department obj = new Department(1, "Books");
+//		Seller seller = new Seller(20, "Bob", "bob@bob", new Date(), 3000.0, obj);
 		SellerDao sellerDao = DaoFactory.createSellerDao(); // programa so conhece o interface 
 		
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
