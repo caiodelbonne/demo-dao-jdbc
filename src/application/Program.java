bd6585daba09e19426c1e51eb7ead30fb1ba2115
CAIO DEL BONNE <caiocesar_01@hotmail.com>
2020-06-30 02:25:30 -0300

    insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -20,7 +21,7 @@ public class Program {
 		System.out.println(seller);
 
 		System.out.println("\n=== TEST 2: seller findByDepartment =====");
-		Department department = new Department(2, null);
+		Department department = new Department(1, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
@@ -33,6 +34,10 @@ public class Program {
 			System.out.println(obj);
 		}
 
+		System.out.println("\n=== TEST 4: seller insert  =====");
+		Seller newSeller = new Seller(null, "Cesar", "greg@email.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Insert new id = "+ newSeller.getId());
 	}
 
 }
